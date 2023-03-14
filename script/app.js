bc984db9f2494d3ff20135574ebfbfac5527af24
Matteo Ena <66940584+matteo-ena@users.noreply.github.com>
2023-03-14 18:52:10 +0100

    change cookie logic

--- script/app.js
+++ script/app.js
@@ -1,8 +1,16 @@
 const CONTENT_BREAKING_NEWS = "BREAKING NEWS: 5 people arrested for non designing responsively ***** Sprite popularity drops even further after new, less sugary, drink introduction.*****"
 
-function acceptCookies() {
+// advertisment mock
+const initAd = () => {
+  const adSpace = document.querySelector('.main__advertising');
+  const gif = '<div style="width:400px;max-width:100%;"><div style="height:0;padding-bottom:52.4%;position:relative;"><iframe width="500" height="262" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameBorder="0" src="https://imgflip.com/embed/6wy03z"></iframe></div></div>';
+  setTimeout(() => adSpace.innerHTML = gif, 3000);
+};
+
+const acceptCookies = () => {
   document.body.classList.remove('no-scroll');
   document.querySelector('.cookieLayer__base').classList.add('cookieLayer__base--accepted');
+  initAd();
 };
 
 const dynamicContent = () => {
@@ -13,18 +21,6 @@ const dynamicContent = () => {
 };
 
 dynamicContent();
-
-// advertisment mock
-const initAd = () => {
-const adSpace = document.querySelector('.main__advertising');
-const gif = '<div style="width:400px;max-width:100%;"><div style="height:0;padding-bottom:52.4%;position:relative;"><iframe width="500" height="262" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameBorder="0" src="https://imgflip.com/embed/6wy03z"></iframe></div></div>';
-  setTimeout(() => adSpace.innerHTML = gif, 3000);
-};
-
-// async CSS
-window.addEventListener('load', () => {
-  initAd();
-});
 
 const cookieLayerButton = document.querySelector('.cookieLayer__button');
 cookieLayerButton.addEventListener('click', acceptCookies());
