b1cb417a44607d6f12b9f02db869f6a2fa73ede4
Matteo Ena <66940584+matteo-ena@users.noreply.github.com>
2023-03-14 18:12:47 +0100

    Modify teasers

--- script/app.js
+++ script/app.js
@@ -1,8 +1,4 @@
 const CONTENT_BREAKING_NEWS = "BREAKING NEWS: 5 people arrested for non designing responsively ***** Sprite popularity drops even further after new, less sugary, drink introduction.*****"
-const CONTENT_ARTICLE_TEASERS = [
-  "This is an article about first party that happened this weekend",
-  "This is an article about second party that happened this weekend",
-  "This is an article about third party that happened this weekend" ];
 
 const acceptCookies = () => {
   document.body.classList.remove('no-scroll');
@@ -10,16 +6,10 @@ const acceptCookies = () => {
 };
 
 const dynamicContent = () => {
-  const teasers = document.querySelectorAll('.teaser__text');
   const marqueeBar = document.querySelector('marquee');
 
   // mock content delay
   setTimeout(() => marqueeBar.innerHTML = CONTENT_BREAKING_NEWS, 3000);
-
-  // create dynamic content
-  teasers.forEach((teaser, index) => {
-    teaser.innerHTML = CONTENT_ARTICLE_TEASERS[index];
-  });
 };
 
 const cookieLayerInit = () => {
