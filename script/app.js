68f9e9e9774620ab8e7e41505fc90ff2bef1bcf8
Matteo Ena <66940584+matteo-ena@users.noreply.github.com>
2023-03-14 19:11:50 +0100

    click event

--- script/app.js
+++ script/app.js
@@ -2,14 +2,12 @@ const CONTENT_BREAKING_NEWS = "BREAKING NEWS: 5 people arrested for non designin
 
 // advertisment mock
 const initAd = () => {
-  console.log('init ad');
   const adSpace = document.querySelector('.main__advertising');
   const gif = '<div style="width:400px;max-width:100%;"><div style="height:0;padding-bottom:52.4%;position:relative;"><iframe width="500" height="262" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameBorder="0" src="https://imgflip.com/embed/6wy03z"></iframe></div></div>';
   setTimeout(() => adSpace.innerHTML = gif, 3000);
 };
 
 const acceptCookies = () => {
-  console.log('accept cookies');
   document.body.classList.remove('no-scroll');
   document.querySelector('.cookieLayer__base').classList.add('cookieLayer__base--accepted');
   initAd();
@@ -25,4 +23,4 @@ const dynamicContent = () => {
 dynamicContent();
 
 const cookieLayerButton = document.querySelector('.cookieLayer__button');
-cookieLayerButton.addEventListener('click', () => this.acceptCookies());
+cookieLayerButton.addEventListener('click', () => acceptCookies());
