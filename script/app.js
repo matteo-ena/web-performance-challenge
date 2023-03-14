3837a795a0256618d2bb12eaa54f359116d87ac0
Matteo Ena <66940584+matteo-ena@users.noreply.github.com>
2023-03-14 18:41:37 +0100

    improvements

--- script/app.js
+++ script/app.js
@@ -2,7 +2,7 @@ const CONTENT_BREAKING_NEWS = "BREAKING NEWS: 5 people arrested for non designin
 
 const acceptCookies = () => {
   document.body.classList.remove('no-scroll');
-  document.querySelector('.cookieLayer__base').classList.add('cookieLayer__base--hidden');
+  document.querySelector('.cookieLayer__base').classList.add('cookieLayer__base--accepted');
 };
 
 const cookieLayerButton = document.querySelector('.cookieLayer__button');
@@ -17,3 +17,15 @@ const dynamicContent = () => {
 };
 
 dynamicContent();
+
+// advertisment mock
+const initAd = () => {
+const adSpace = document.querySelector('.main__advertising');
+const gif = '<div style="width:400px;max-width:100%;"><div style="height:0;padding-bottom:52.4%;position:relative;"><iframe width="500" height="262" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameBorder="0" src="https://imgflip.com/embed/6wy03z"></iframe></div></div>';
+  setTimeout(() => adSpace.innerHTML = gif, 3000);
+};
+
+// async CSS
+window.addEventListener('load', () => {
+  initAd();
+});
