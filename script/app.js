bdaf706e4068c656589bdb4231730cec411bd5a2
Matteo Ena <66940584+matteo-ena@users.noreply.github.com>
2023-03-14 18:27:11 +0100

    js modfication

--- script/app.js
+++ script/app.js
@@ -1,13 +1,13 @@
 const CONTENT_BREAKING_NEWS = "BREAKING NEWS: 5 people arrested for non designing responsively ***** Sprite popularity drops even further after new, less sugary, drink introduction.*****"
 
-const cookieLayerButton = document.querySelector('.cookieLayer__button');
-
-cookieLayerButton.addEventListener('click', acceptCookies());
-
 const acceptCookies = () => {
   document.body.classList.remove('no-scroll');
   document.querySelector('.cookieLayer__base').classList.add('cookieLayer__base--accepted');
 };
+
+const cookieLayerButton = document.querySelector('.cookieLayer__button');
+
+cookieLayerButton.addEventListener('click', acceptCookies());
 
 const dynamicContent = () => {
   const marqueeBar = document.querySelector('marquee');
